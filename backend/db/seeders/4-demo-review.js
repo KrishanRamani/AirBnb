df89469ed8eb2ce57e8b2de40c0d2acb9358e55b
krishan ramani <ar.ramani.krishan@gmail.com>
2022-07-26 20:39:50 -0700

    migration changes

--- backend/db/seeders/4-demo-review.js
+++ backend/db/seeders/4-demo-review.js
@@ -26,32 +26,30 @@ const reviews = [
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    //   return queryInterface.bulkInsert('Reviews', [
+    await queryInterface.bulkInsert('Reviews', reviews
+      , {});
+    // for (let review_info of reviews) {
+    //   const {
+    //     review,
+    //     stars
+    //   } = review_info;
 
-    // ], {});
-    for (let review_info of reviews) {
-      const {
-        review,
-        stars
-      } = review_info;
+    //   const user = await User.findByPk(1);
+    //   const spot = await Spot.findByPk(1);
 
-      const user = await User.findByPk(1);
-      const spot = await Spot.findByPk(1);
-
-      await user.createReview({
-        user_id: user.id,
-        spot_id: spot.id,
-        review,
-        stars
-      });
-    }
+    //   await user.createReview({
+    //     user_id: user.id,
+    //     spot_id: spot.id,
+    //     review,
+    //     stars
+    //   });
 
   },
 
 
   down: async (queryInterface, Sequelize) => {
     //const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Reviews');
+    await queryInterface.bulkDelete('Reviews');
     //, {
     //firstname: { [Op.in]: ['Demo', 'Fake1', 'Fake2'] },
     //lastname: { [Op.in]: ['Lition', 'Khalifa', 'Mia'] }
